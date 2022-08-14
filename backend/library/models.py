2bbca6a88b197ccf81fa0235a8031b4e2275d780
kodzyz <kodzyzdok@gmail.com>
2022-08-14 16:43:06 +0300

    python manage.py makemigrations

--- backend/library/models.py
+++ backend/library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.birthday_year})'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    authors = models.ManyToManyField(Author)
