72c32dcc5343d60f3c1e38b745c326a6567991d2
kodzyz <kodzyzdok@gmail.com>
2022-08-27 18:14:14 +0300

    0:43 запрет события onSubmit

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,6 @@
-//0:39 обращаемся к параметру target ->name="login" или name="password"
+//0:43 нажатие на кнопку Login вызывает перезагрузку приложения js
+//запретим кнопке работать как submit - выполнение GET запроса
+
 import React from 'react'
 
 class LoginForm extends React.Component {
@@ -18,10 +20,15 @@ class LoginForm extends React.Component {
         })
     }
 
+    handleSubmit(event) {
+        console.log(this.state.login, this.state.password) // что сохранилось в состоянии при нажатии на Login
+        event.preventDefault() // запрет событий по умолчанию в браузере
+    }
+
     render () {
         return (
             <div>
-                <form>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
                     <input type="text" name="login" placeholder="login" value={this.state.login} onChange={(event) => this.handleChange(event)} />
                     <input type="password" name="password" placeholder="password" value={this.state.password} onChange={(event) => this.handleChange(event)} />
                     <input type="submit" value="Login" />
