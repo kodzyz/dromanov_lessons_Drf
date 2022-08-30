a4a16e4d37948e0b3a89ffd42191ab0246490e5f
kodzyz <kodzyzdok@gmail.com>
2022-08-30 22:42:47 +0300

    0:48 APIRequestFactory 401 != 200

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
-# python manage.py test
-# 0:47 Run/Debug Configuation - запуск тестов через отладчик
-
-from rest_framework.test import APIRequestFactory
+# конструируем request для наших запросов
+# передаём запрос в view
+# после передачи запроса получаем ответ response и проверяем статус
+from rest_framework.test import APIRequestFactory  # класс для генерации запросов
+from .views import AuthorModelViewSet
+from rest_framework import status
 
 
 class AuthorTestCase(TestCase):
     def test_get_list(self):  # список авторов
-        pass
+        factory = APIRequestFactory()
+        request = factory.get('api/authors/')  # конструируем GET запрос по URL
+        view = AuthorModelViewSet.as_view({'get': 'list'})  # связать запрос get с методом list
+        response = view(request)  # запускаем views от запроса
+        # проверка через предположение что статус = 200
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # AssertionError: 401 != 200 -> IsAuthenticated
