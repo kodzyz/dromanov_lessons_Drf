90c8b367d403f635109a7a958adb38b9f7c00068
kodzyz <kodzyzdok@gmail.com>
2022-08-30 23:19:47 +0300

    0:58 create_superuser force_authenticate

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,15 +1,19 @@
 from django.test import TestCase
-#
-from rest_framework.test import APIRequestFactory  # класс для генерации запросов
+# создаем пользователя
+# логинимся: аутентификация
+from rest_framework.test import APIRequestFactory, force_authenticate
 from .views import AuthorModelViewSet
 from rest_framework import status
+from django.contrib.auth.models import User
 
 
 class AuthorTestCase(TestCase):
     def test_get_list(self):
         factory = APIRequestFactory()
         request = factory.get('/api/authors/')
+        user = User.objects.create_superuser(username='root', password='1234')
+        force_authenticate(request, user=user)
         view = AuthorModelViewSet.as_view({'get': 'list'})
         response = view(request)
-        # AssertionError: 401 != 200 потому что не залогинились -> permissions.IsAuthenticated
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)  # не залогинились = 401
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
