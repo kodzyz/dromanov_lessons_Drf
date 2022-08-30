cf413488181f3e211c963f8e06a04adb080c10a4
kodzyz <kodzyzdok@gmail.com>
2022-08-30 23:04:39 +0300

    0:53 APIRequestFactory HTTP_401_UNAUTHORIZED

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,17 +1,15 @@
 from django.test import TestCase
-# конструируем request для наших запросов
-# передаём запрос в view
-# после передачи запроса получаем ответ response и проверяем статус
+#
 from rest_framework.test import APIRequestFactory  # класс для генерации запросов
 from .views import AuthorModelViewSet
 from rest_framework import status
 
 
 class AuthorTestCase(TestCase):
-    def test_get_list(self):  # список авторов
+    def test_get_list(self):
         factory = APIRequestFactory()
-        request = factory.get('api/authors/')  # конструируем GET запрос по URL
-        view = AuthorModelViewSet.as_view({'get': 'list'})  # связать запрос get с методом list
-        response = view(request)  # запускаем views от запроса
-        # проверка через предположение что статус = 200
-        self.assertEqual(response.status_code, status.HTTP_200_OK)  # AssertionError: 401 != 200 -> IsAuthenticated
+        request = factory.get('/api/authors/')
+        view = AuthorModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        # AssertionError: 401 != 200 потому что не залогинились -> permissions.IsAuthenticated
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)  # не залогинились = 401
