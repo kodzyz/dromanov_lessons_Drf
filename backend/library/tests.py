2dc6b9102e0dd70371c32de33cbb2fafc9f1d114
kodzyz <kodzyzdok@gmail.com>
2022-08-30 22:14:07 +0300

    0:43 запуск тестов

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+# python manage.py test
+# 0:47 Run/Debug Configuation - запуск тестов через отладчик
 
-# Create your tests here.
+from rest_framework.test import APIRequestFactory
+
+
+class AuthorTestCase(TestCase):
+    def test_get_list(self):  # список авторов
+        pass
