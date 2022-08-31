e3da95579c83d1b153968c3196ff937fdcd4bc01
kodzyz <kodzyzdok@gmail.com>
2022-08-31 22:56:48 +0300

    1:31 POST

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -5,18 +5,24 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from .models import Author
 
+# POST-запрос создания модели
 
 class AuthorClientTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(username='root', password='1234')
         self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
 
-    def test_get_list(self):
-        self.client.login(username='root', password='1234')  # знаем логин и пароль
-        response = self.client.get('/api/authors/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.client.logout()
-        response = self.client.get('/api/authors/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_post(self):
+        self.client.login(username='root', password='1234')
+        #self.client.force_login(user=self.user)
+        response = self.client.post('/api/authors/', {
+            "first_name": "Александр",
+            "last_name": "Грин",
+            "birthday_year": 1860
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        author = Author.objects.get(pk=response.data.get('id'))
+        self.assertEqual(author.last_name, 'Грин')
+
+
 
