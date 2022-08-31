94097fa401b634bf9c04615d08c37b86d4bd2161
kodzyz <kodzyzdok@gmail.com>
2022-08-31 21:54:33 +0300

    1:10 APIClient

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
-# setUp - метод выполн перед каждым запуском теста
-# tearDown - вып после теста
-
-# эта схема не тестирует роутер
-from rest_framework.test import APIRequestFactory, force_authenticate
+# тестирование запросов целиком
+# отправлять запросы на определённый адрес и сразу получать ответ
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
 from .views import AuthorModelViewSet
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -11,13 +9,9 @@ from .models import Author
 
 
 class AuthorTestCase(TestCase):
-
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(username='root', password='1234')
         self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
-
-    # def tearDown(self) -> None:
-    #         pass
 
     def test_get_list(self):
         factory = APIRequestFactory()
@@ -27,5 +21,14 @@ class AuthorTestCase(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        print(
-            response.data)
+
+
+class AuthorClientTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()  # полноценная эмуляция клиента
+        self.user = User.objects.create_superuser(username='root', password='1234')
+        self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
+
+    def test_get_list(self):
+        response = self.client.get('/api/authors/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
