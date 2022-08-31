ead70fd6c92de90d3ef754034be9c6b7df9440ff
kodzyz <kodzyzdok@gmail.com>
2022-08-31 23:33:59 +0300

    1:43 mixer

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,21 +1,28 @@
+import random
+
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, force_authenticate, APITestCase
 from .views import AuthorModelViewSet
 from rest_framework import status
 from django.contrib.auth.models import User
 from .models import Author
+from mixer.backend.django import mixer
 
-# POST-запрос создания модели
+
+# генерация тестовых данных (автоматически)
+# pip install mixer
 
 class AuthorClientTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(username='root', password='1234')
-        self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
+        #self.author = mixer.blend(Author)
+        # self.author = mixer.cycle(5).blend(Author) # 5 авторов
+        # self.author = mixer.blend(Author, birthday_year=1799)  # одно поле генерится с конкретным заначением
+        self.author = mixer.blend(Author, birthday_year=mixer.sequence(lambda c: int(random.random()*2000)))  # от 0 до 2000
 
     def test_post(self):
         self.client.login(username='root', password='1234')
-        #self.client.force_login(user=self.user)
-        response = self.client.post('/api/authors/', {
+        response = self.client.post('/api/authors/', {  # debug 
             "first_name": "Александр",
             "last_name": "Грин",
             "birthday_year": 1860
@@ -23,6 +30,3 @@ class AuthorClientTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         author = Author.objects.get(pk=response.data.get('id'))
         self.assertEqual(author.last_name, 'Грин')
-
-
-
