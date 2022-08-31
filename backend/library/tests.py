67e9d2ba02e9d261c5e94d051d0d26dae874800a
kodzyz <kodzyzdok@gmail.com>
2022-08-31 22:08:52 +0300

    1:19 APITestCase authenticate

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -7,26 +7,14 @@ from django.contrib.auth.models import User
 from .models import Author
 
 
-class AuthorTestCase(TestCase):
-    def setUp(self) -> None:
-        self.user = User.objects.create_superuser(username='root', password='1234')
-        self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
-
-    def test_get_list(self):
-        factory = APIRequestFactory()
-        request = factory.get('/api/authors/')
-        force_authenticate(request, user=self.user)
-        view = AuthorModelViewSet.as_view({'get': 'list'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-
-
 class AuthorClientTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(username='root', password='1234')
         self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
 
     def test_get_list(self):
+        self.client.force_authenticate(self.user)  # аутентификация пользователя
         response = self.client.get('/api/authors/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
