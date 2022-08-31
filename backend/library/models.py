fcabf23f5cc34a70aa86f80c388d4a68783fc5ba
kodzyz <kodzyzdok@gmail.com>
2022-09-01 00:04:35 +0300

    1:50 mixer в связанных моделях

--- backend/library/models.py
+++ backend/library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from rest_framework.authtoken.models import Token
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
@@ -13,3 +14,10 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=64)
     authors = models.ManyToManyField(Author)
+
+
+class Bio(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+#makemigrations
+#migrate
--- backend/library/tests.py
+++ backend/library/tests.py
@@ -5,24 +5,19 @@ from rest_framework.test import APIRequestFactory, force_authenticate, APITestCa
 from .views import AuthorModelViewSet
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import Author
+from .models import Author, Bio
 from mixer.backend.django import mixer
 
-
-# генерация тестовых данных (автоматически)
-# pip install mixer
 
 class AuthorClientTestCase(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(username='root', password='1234')
-        #self.author = mixer.blend(Author)
-        # self.author = mixer.cycle(5).blend(Author) # 5 авторов
-        # self.author = mixer.blend(Author, birthday_year=1799)  # одно поле генерится с конкретным заначением
-        self.author = mixer.blend(Author, birthday_year=mixer.sequence(lambda c: int(random.random()*2000)))  # от 0 до 2000
+        self.author = mixer.blend(Author)
+        self.bio = mixer.blend(Bio)  # сгенерит вложенные модели
 
     def test_post(self):
         self.client.login(username='root', password='1234')
-        response = self.client.post('/api/authors/', {  # debug 
+        response = self.client.post('/api/authors/', {  # debug
             "first_name": "Александр",
             "last_name": "Грин",
             "birthday_year": 1860
