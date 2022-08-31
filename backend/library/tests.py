f7fb75321f38ad4696ce03b9ae24e5621021d354
kodzyz <kodzyzdok@gmail.com>
2022-08-31 21:39:22 +0300

    1:06 setUp,tearDown

--- backend/library/tests.py
+++ backend/library/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-# создаем автора
+# setUp - метод выполн перед каждым запуском теста
+# tearDown - вып после теста
+
+# эта схема не тестирует роутер
 from rest_framework.test import APIRequestFactory, force_authenticate
 from .views import AuthorModelViewSet
 from rest_framework import status
@@ -8,14 +11,21 @@ from .models import Author
 
 
 class AuthorTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_superuser(username='root', password='1234')
+        self.author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
+
+    # def tearDown(self) -> None:
+    #         pass
+
     def test_get_list(self):
         factory = APIRequestFactory()
         request = factory.get('/api/authors/')
-        user = User.objects.create_superuser(username='root', password='1234')
-        author = Author.objects.create(first_name='Александр', last_name='Пушкин', birthday_year=1799)
-        force_authenticate(request, user=user)
+        force_authenticate(request, user=self.user)
         view = AuthorModelViewSet.as_view({'get': 'list'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-        print(response.data)  # [OrderedDict([('id', 1), ('first_name', 'Александр'), ('last_name', 'Пушкин'), ('birthday_year', 1799)])]
+        print(
+            response.data)
