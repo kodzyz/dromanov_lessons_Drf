5e3652c44691a533684c0dcd98dfa11ff4634c1e
kodzyz <kodzyzdok@gmail.com>
2022-09-07 21:29:56 +0300

    0:40 связь с models Author

--- backend/library/graphql.py
+++ backend/library/graphql.py
@@ -1,24 +1,29 @@
 import graphene
+from graphene_django import DjangoObjectType
+from .models import Author
+
+
+# связь с моделью = похоже на serializer
+class AuthorObjectType(DjangoObjectType):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String()  # команда hello, return тип данных String
+    all_authors = graphene.List(AuthorObjectType)  # команда return List авторов
 
-    # обработчик команды
-    def resolve_hello(self, info):  # info - информация о схеме вызова
-        return 'world!'
+    def resolve_all_authors(self, info):
+        return Author.objects.all()
 
 
 schema = graphene.Schema(query=Query)
 
-# http://127.0.0.1:8000/graphql/
-# вид запроса
 # {
-#     hello
-# }
-# ответ
-# {
-#   "data": {
-#     "hello": "world!"
+#   allAuthors {
+#     id
+#     firstName
+#     lastName
+#     birthdayYear
 #   }
 # }
