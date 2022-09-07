2f6e17cf197195425480909edbaf71677f959f52
kodzyz <kodzyzdok@gmail.com>
2022-09-07 22:13:44 +0300

    0:53 graphene + связанные модели

--- backend/library/graphql.py
+++ backend/library/graphql.py
@@ -1,26 +1,56 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Author
+from .models import Author, Book
 
 
-# связь с моделью = похоже на serializer
 class AuthorObjectType(DjangoObjectType):
     class Meta:
         model = Author
         fields = '__all__'
 
 
+class BookObjectType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
 class Query(graphene.ObjectType):
-    all_authors = graphene.List(AuthorObjectType)  # команда return List авторов
+    all_authors = graphene.List(AuthorObjectType)
 
     def resolve_all_authors(self, info):
         return Author.objects.all()
+
+    all_books = graphene.List(BookObjectType)
+
+    def resolve_all_books(self, info):
+        return Book.objects.all()
 
 
 schema = graphene.Schema(query=Query)
 
 # {
-#   allAuthors {
+#    allBooks {
+#     id
+#     title
+#     authors{
+#       id
+#       firstName
+#       lastName
+#       birthdayYear
+#     }
+#   }
+# }
+
+# {
+#   allBooks{
+#     id
+#     title
+#     authors{
+#       id
+#     }
+#   }
+#   allAuthors{
 #     id
 #     firstName
 #     lastName
