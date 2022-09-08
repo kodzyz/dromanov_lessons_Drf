8fe5fa268e4d9d833dbcd03b032f46b6d20e34fb
kodzyz <kodzyzdok@gmail.com>
2022-09-08 20:04:57 +0300

    1:20 - получение автора по имени

--- backend/library/graphql.py
+++ backend/library/graphql.py
@@ -27,18 +27,43 @@ class Query(graphene.ObjectType):
         # return Book.objects.all()
         return Author.objects.get(pk=1).book_set.all()
 
-    # {
-    #   allBooks{title}
-    # }
-
     # получение автора по id
     get_author_by_id = graphene.Field(AuthorObjectType, pk=graphene.Int(required=True))
 
     def resolve_get_author_by_id(self, info, pk):  # получает id
         return Author.objects.get(pk=pk)
 
-    # получение более одной модели
+    # получение автора по имени
+    get_author_by_name = graphene.List(AuthorObjectType,
+                                       first_name=graphene.String(required=False),
+                                       last_name=graphene.String(required=False)
+                                       )
 
+    def resolve_get_author_by_name(self, info, first_name=None, last_name=None):
+        if not first_name and not last_name:
+            return Author.objects.all()
+        params = {}
+        if first_name:
+            params['first_name__contains'] = first_name  # params['first_name'] полное/частичное совпадение
+        if last_name:
+            params['last_name__contains'] = last_name  # params['last_name']
+        return Author.objects.filter(**params)
+
+
+# {
+#   getAuthorByName(firstName:"Александр", lastName:"Грин"){
+#     id
+#     firstName
+#     lastName
+#   }
+# }
+
+# {
+#   getAuthorByName(lastName:"Пуш"){
+#     id
+#     firstName
+#     lastName
+#   }
+# }
 
 schema = graphene.Schema(query=Query)
-
--- backend/library/models.py
+++ backend/library/models.py
@@ -13,27 +13,11 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=64)
-    authors = models.ManyToManyField(Author)  # под капотом связи ManyToMany
-    # обратная связь - внутри Author появляется доп.поле 'book_Set'
-    # набор книжек в котором есть ссылка на данного автора
+    authors = models.ManyToManyField(Author)
 
-    # (Author, related_name=) изменяет название поля 'book_Set'
-
-# {
-#   getAuthorById(pk:1){
-#     id
-#     firstName
-#     lastName
-#     birthdayYear
-#     bookSet{
-#       id
-#       title
-#     }
-#   }
-# }
 
 class Bio(models.Model):
     title = models.CharField(max_length=64)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-#makemigrations
-#migrate
+# makemigrations
+# migrate
